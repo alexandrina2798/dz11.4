be19f8cde38662bcd70ce34be78f7ccd80b7cc0f
alexandrina2798 <alexandrina2798@mail.ru>
2021-04-12 23:19:47 +0300

    delete comments

--- src/main/java/generators/FioGenerator.java
+++ src/main/java/generators/FioGenerator.java
@@ -22,37 +22,11 @@ public class FioGenerator {
     public final FIO generateParams(final int code) {
         final int lastNameIndex = getDigitsSum(code);
         final String sex = (lastNameIndex % 2 == 0) ? "f" : "m";
-        //setLastNameFromFile(lastNameIndex, sex);
-        //setFirstNameFromFile(getDigitsSum(code / 100), sex);
-        //setMiddleNameFromFile(getDigitsSum(code % 100), sex);
         lastName = getLinesFromFile("lastNames_" + sex).get(getDigitsSum(code));
         firstName = getLinesFromFile("names_" + sex).get(getDigitsSum(code / 100));
         middleName = getLinesFromFile("middleNames_" + sex).get(getDigitsSum(code % 100));
         return new FIO(lastName, firstName, middleName);
     }
-/*
-    public final String getLastName() {
-        return lastName;
-    }
 
-    public final String getFirstName() {
-        return firstName;
-    }
-
-    public final String getMiddleName() {
-        return middleName;
-    }
-
-    private void setLastNameFromFile(final int i, final String sex) {
-        lastName = getLinesFromFile("lastNames_" + sex).get(i);
-    }
-
-    private void setFirstNameFromFile(final int i, final String sex) {
-        firstName = getLinesFromFile("names_" + sex).get(i);
-    }
-
-    private void setMiddleNameFromFile(final int i, final String sex) {
-        middleName = getLinesFromFile("middleNames_" + sex).get(i);
-    }*/
 }
 
