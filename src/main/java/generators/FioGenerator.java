83de8e016521a20d63ec6316a56d0029ef430a76
alexandrina2798 <alexandrina2798@mail.ru>
2021-04-12 23:25:03 +0300

    fix DTO FIO

--- src/main/java/generators/FioGenerator.java
+++ src/main/java/generators/FioGenerator.java
@@ -5,10 +5,6 @@ import static utils.MyMath.getDigitsSum;
 
 public class FioGenerator {
 
-    private String lastName;
-    private String firstName;
-    private String middleName;
-
     /**
      * ФИО берутся из соответствующих файлов по индексу в листе:
      * Фамилия - сумма цифр в коде
@@ -22,10 +18,10 @@ public class FioGenerator {
     public final FIO generateParams(final int code) {
         final int lastNameIndex = getDigitsSum(code);
         final String sex = (lastNameIndex % 2 == 0) ? "f" : "m";
-        lastName = getLinesFromFile("lastNames_" + sex).get(getDigitsSum(code));
-        firstName = getLinesFromFile("names_" + sex).get(getDigitsSum(code / 100));
-        middleName = getLinesFromFile("middleNames_" + sex).get(getDigitsSum(code % 100));
-        return new FIO(lastName, firstName, middleName);
+        FIO person = new FIO(getLinesFromFile("lastNames_" + sex).get(getDigitsSum(code)),
+                getLinesFromFile("names_" + sex).get(getDigitsSum(code / 100)),
+                getLinesFromFile("middleNames_" + sex).get(getDigitsSum(code % 100)));
+        return person;
     }
 
 }
